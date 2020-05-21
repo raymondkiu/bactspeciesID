6aacba6d4f74a29bc310a83893bff2e64f8572c3
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2020-05-21 12:09:27 +0100

    Bump up to v1.1
    Added the option to change BLASTn coverage (default:50%)

--- bin/speciesID.sh
+++ bin/speciesID.sh
@@ -7,28 +7,31 @@ usage () {
   echo ""
   echo "Usage: $0 [options] FASTA"
   echo "Option:"
-  echo " -i BLAST identity (default:99)"
+  echo " -i BLASTn identity (default:99)"
+  echo " -c BLASTn coverage (default:50)"
   echo " -d ABRicate database (default:SILVA-16S)"
   echo " -h print usage and exit"
   echo " -a print author and exit"
   echo " -v print version and exit"
   echo ""
-  echo "Version 1.0 (2020)"
+  echo "Version 1.1 (2020)"
   echo "Author: Raymond Kiu Raymond.Kiu@quadram.ac.uk"
   echo "";
 }
-version () { echo "version 1.0 (2020)";}
+version () { echo "version 1.1 (2020)";}
 author () { echo "Author: Raymond Kiu Raymond.Kiu@quadram.ac.uk";}
 
 #dafault value for BLAST identity and database dor ABRicate
 
 IDENTITY=99
+COVERAGE=50
 DATABASE=SILVA-16S
 
 # parse the options
-while getopts 'i:d:hav' opt;do
+while getopts 'i:c:d:hav' opt;do
   case $opt in
     i) IDENTITY=$OPTARG ;;
+    c) COVERAGE=$OPTARG ;;
     d) DATABASE=$OPTARG ;;
     h) usage; exit;;
     a) author; exit;;
@@ -45,7 +48,8 @@ shift $((OPTIND-1))
 # check for mandatory positional parameters, only 1 positional argument will be checked
 if [ $# -lt 1 ]; then
    echo "Usage: $0 [options] FASTA"
-   echo "Option: -i BLAST identity (default:$IDENTITY)"
+   echo "Option: -i BLASTn identity (default:$IDENTITY)"
+   echo "Option: -c BLASTn coverage (default:$COVERAGE)"
    echo "Option: -d ABRicate database (default:$DATABASE)"
    echo ""
    exit 1
@@ -53,7 +57,7 @@ fi
 
 fasta=$1
 
-echo "will identify with BLAST identity $IDENTITY% with ABRicate database $DATABASE"
+echo "will identify with BLASTn identity $IDENTITY% and BLASTn coverage $COVERAGE% with ABRicate database $DATABASE"
 echo "identifying 16S rRNA genes from genome assembly $fasta ..."
 barrnap --quiet $1 > $fasta-gff
 echo "16S rRNA genes have now been identified"
@@ -79,7 +83,7 @@ rm $fasta-16S-id.txt
 
 echo "16S rRNA gene sequence is now stored in $fasta-16S.fna"
 echo "identifying species using ABRicate $DATABASE database at identity >$IDENTITY%..."
-abricate --quiet --db $DATABASE --minid=$IDENTITY $fasta-16S.fna|grep -v "#FILE" | awk '{print $13" "$14}' > $fasta.species
+abricate --quiet --db $DATABASE --mincov=$COVERAGE --minid=$IDENTITY $fasta-16S.fna|grep -v "#FILE" | awk '{print $13" "$14}' > $fasta.species
 
 echo "Species identified is now stored in $fasta.species"
 echo "Removing intermediary file..."
